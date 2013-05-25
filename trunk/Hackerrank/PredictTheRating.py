d23cbea3fd3b045356a4280c945ea5b30fec1669
steve3003@hotmail.it <steve3003@hotmail.it@901313ad-d5d4-445f-b086-fdd6cf6ee323>
2013-05-25 18:22:35 +0000

    Added predict the rating contest

--- trunk/Hackerrank/PredictTheRating.py
+++ trunk/Hackerrank/PredictTheRating.py
@@ -3,7 +3,26 @@ Created on 25/mag/2013
 
 @author: Stefano
 '''
-import numpy
+from numpy import asarray, dot, transpose
+from numpy.linalg import inv
 
-M = [[1, 2], [3, 4]]
-print numpy.linalg.det(M)
+n=int(raw_input())
+for i in range(0,n):
+    y=[int(j) for j in raw_input().strip().split()]
+    na=int(y[0]/2)
+    y.pop(0)
+    y=y[::-1]
+    phi=[]
+    yf=[]
+    for index in range(1,len(y)-na+1):
+        phi.append(y[index:index+na])
+        yf.append([y[index-1]])
+    phi=asarray(phi)
+    yf=asarray(yf)
+    phiT=transpose(phi)
+    theta=dot(dot(inv(dot(phiT,phi)),phiT),yf)
+    yNext=dot(y[0:na],theta)
+    if yNext>y[0]:
+        print 1
+    else:
+        print 0
