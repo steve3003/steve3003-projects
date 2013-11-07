cdb7e4206268059aa8b378c5f845119c55eeee98
steve3003@hotmail.it <steve3003@hotmail.it@901313ad-d5d4-445f-b086-fdd6cf6ee323>
2013-11-07 17:08:17 +0000

    Added multi cards picker

--- Scopa/src/Card.py
+++ Scopa/src/Card.py
@@ -3,25 +3,27 @@ Created on 04/nov/2013
 
 @author: Stefano
 '''
+SEEDS = {0:"d", 1:"b", 2:"s", 3:"c"}
 
-class Card(object):
-    SEEDS = {0:"d", 1:"b", 2:"s", 3:"c"}
-    
+class Card(object): 
     def __init__(self, number, seed):
         self.number = number
         self.seed = seed
         self.id = Card.get_id(self.number, self.seed)
         
     def __repr__(self, *args, **kwargs):
-        return str(self.number+1) + self.SEEDS[self.seed]
+        return str(self.number) + SEEDS[self.seed]
+    
+    def can_pick(self, l):
+        return self.number == sum([i.number for i in l])
 
     @classmethod
     def get_id(cls, number, seed):
-        return seed * 10 + number
+        return seed * 10 + (number - 1)
     
     @classmethod
     def get_Card(cls, idn):
-        return Card(idn % 10, int(idn / 10))
+        return Card((idn % 10) + 1, int(idn / 10))
     
     @classmethod
     def toCards(cls, l):
